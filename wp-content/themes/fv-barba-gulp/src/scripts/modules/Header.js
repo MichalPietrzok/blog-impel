91519e79dcb2144c6ba013821a66c99a98bbf714
jura <yury.andreichuk@follow.vision>
2022-11-01 17:33:52 +0100

    add bread crumps to all subpages / add click actions to mobile menu

--- wp-content/themes/fv-barba-gulp/src/scripts/modules/Header.js
+++ wp-content/themes/fv-barba-gulp/src/scripts/modules/Header.js
@@ -3,6 +3,19 @@ class Header {
     this.mainMenuItems = document.querySelectorAll('.header-main__item')
     this.mainNav = document.querySelector('#main-nav ul')
     this.mainNavFirstItem = this.mainNav.querySelector('li')
+    this.parentsItems = this.mainNav.querySelectorAll('.menu-item-has-children')
+    this.emptyLinks = this.mainNav.querySelectorAll('a[href="#"]')
+
+    this.emptyLinks.forEach(link => {
+      link.addEventListener('click', (e) => { e.preventDefault() })
+    })
+
+    this.parentsItems.forEach(item => {
+      item.addEventListener('click', (e) => {
+        e.stopPropagation()
+        this.swithMenu(item, 'toggle')
+      })
+    })
     this.addMobileMenuItems()
   }
 
@@ -12,6 +25,19 @@ class Header {
       proxyItem.classList.add('d-xl-none')
       this.mainNav.appendChild(proxyItem)
     })
+  }
+
+  swithMenu(item, mod) {
+    const currentMenu = item.querySelector('.sub-menu')
+    const currentLink = item.querySelector('a[href="#"]')
+    const subMenus = currentMenu.querySelectorAll('.menu-item-has-children')
+    currentMenu.classList[mod]('active')
+    currentLink.classList[mod]('active')
+    if (subMenus.length) {
+      subMenus.forEach(subMenu => {
+        this.swithMenu(subMenu, 'remove')
+      })
+    }
   }
 }
 
